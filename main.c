1bda79b11f31fa19d7a3bc013212d4fca2e9c793
daniielcosta <120035352+daniielcosta@users.noreply.github.com>
2024-11-19 14:24:39 -0300

    Exercício resolvido

--- main.c
+++ main.c
@@ -2,8 +2,47 @@
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int linhas, colunas, i, j;
+    int **matriz;
+    
+    scanf("%d %d", &linhas, &colunas);
+    
+    if (linhas <= 0 || colunas <= 0) {
+        printf("[vazio]\n");
+        return 0;
+    }
+
+    matriz = (int **)malloc(linhas * sizeof(int *));
+    if (matriz == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
+    }
+
+    for (i = 0; i < linhas; i++) {
+        matriz[i] = (int *)malloc(colunas * sizeof(int));
+        if (matriz[i] == NULL) {
+            printf("Erro ao alocar memória.\n");
+            return 1;
+        }
+    }
+
+    for (i = 0; i < linhas; i++) {
+        for (j = 0; j < colunas; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (i = 0; i < linhas; i++) {
+        for (j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
 
     return 0;
 }
